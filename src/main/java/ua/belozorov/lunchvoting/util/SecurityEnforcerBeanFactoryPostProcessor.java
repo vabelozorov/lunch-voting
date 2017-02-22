a966be3ad0299ee871bd91a6a82249a4b3593126
Belozorov Vladimir <va.belozorov@gmail.com>
2017-02-22 03:30:19 +0200

    Security done. Implemented BFPP for enforcing security on @RestController-s along with @Insecure for allowing insecure methods

---
+++ src/main/java/ua/belozorov/lunchvoting/util/SecurityEnforcerBeanFactoryPostProcessor.java
@@ -0,0 +1,9 @@
+package ua.belozorov.lunchvoting.util;
+
+/**
+ * <h2></h2>
+ *
+ * @author vabelozorov on 22.02.17.
+ */
+public class SecurityEnforcerBeanFactoryPostProcessor {
+}
---
+++ src/test/java/ua/belozorov/lunchvoting/service/user/UserProfileServiceImplTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * <h2></h2>
+ *
+ * @author vabelozorov on 21.02.17.
+ */
+public class UserProfileServiceImplTest {
+
+}
